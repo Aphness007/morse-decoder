dc348c3d424e6eed1a8b6bb074f1ec83e0e82045
aphness007 <aphness007@gmail.com>
2022-08-10 14:48:49 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -39,6 +39,25 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let array = [];
+    expr.match(/.{1,10}/g).forEach(el => {
+        array.push(el.replace(/10/g,'.')
+        .replace(/11/g,'-')
+        .replace(/0/g,''));
+    });
+
+    for(let key in MORSE_TABLE){
+        // console.log(key)
+        // console.log(MORSE_TABLE[key]);
+        for(let i = 0; i < array.length; i++){
+            if(key == array[i]){
+                array[i] = MORSE_TABLE[key];
+            }
+        }
+    }
+
+    
+    return array.join('').replaceAll('**********',' ');
 }
 
 module.exports = {
